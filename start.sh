47a465d778ad68dc722b2fdc2f0d54a573ce9033
Keegan White <keeganthomaswhite@gmail.com>
2023-11-14 11:43:18 +0200

    updated start.sh to create bridge and about to test

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ read -p "What domain are you using? (e.g. example.com) " DOMAIN
 echo "TRAEFIK_BRIDGE=$TRAEFIK_BRIDGE" > .env
 echo "DOMAIN=$DOMAIN" >> .env
 
+docker network create --driver bridge $TRAEFIK_BRIDGE
 docker compose config
 docker compose up
