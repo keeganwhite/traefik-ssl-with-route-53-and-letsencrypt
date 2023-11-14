99d0571b50f5d0a2312219f6f41fe9a02a286a24
Keegan White <keeganthomaswhite@gmail.com>
2023-11-14 12:23:08 +0200

    updated start,sh

--- start.sh
+++ start.sh
@@ -7,5 +7,6 @@ echo "TRAEFIK_BRIDGE=$TRAEFIK_BRIDGE" > .env
 echo "DOMAIN=$DOMAIN" >> .env
 
 docker network create --driver bridge $TRAEFIK_BRIDGE
+echo 'This may take some time... Running build now... You can check the logs for errors using: docker logs traefik-ssl'
 docker compose config
-docker compose up
+docker compose up -d
